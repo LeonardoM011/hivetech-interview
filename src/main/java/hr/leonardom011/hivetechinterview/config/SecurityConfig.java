479bfc3fc5d58334e3abc9ce239a1e63fef77bc3
Leonardo Marinovic <leonardo.leo.201@gmail.com>
2025-05-17 21:32:43 +0200

    Added problem and Security

---
+++ src/main/java/hr/leonardom011/hivetechinterview/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package hr.leonardom011.hivetechinterview.config;
+
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static hr.leonardom011.hivetechinterview.constant.SecurityConstant.JWT_AUTH_NAME;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((requests) -> requests
+                        .anyRequest().permitAll()
+                )
+                .csrf(AbstractHttpConfigurer::disable);
+        return http.build();
+    }
+}
---
+++ src/main/java/hr/leonardom011/hivetechinterview/constant/SecurityConstant.java
@@ -0,0 +1,5 @@
+package hr.leonardom011.hivetechinterview.constant;
+
+public class SecurityConstant {
+    public static final String JWT_AUTH_NAME = "bearerAuth";
+}
